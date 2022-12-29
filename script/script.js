b19cb7caee9d4fbd8ab9077b51c2add0a8283ddc
Andrey Romashev <andreygaranin.0@yandex.ru>
2022-12-29 15:39:02 +0700

    исправление ошибок

--- script/script.js
+++ script/script.js
@@ -1,38 +1,33 @@
-let formElement = document.querySelector('.popup'); // выбираем форму
 
-// узнаем начальные данные пользователя
 let profile = document.querySelector('.profile');
 let name = profile.querySelector('.profile__name');
 let job = profile.querySelector('.profile__job');
-
-// открыть popup
 let btnEdit = profile.querySelector('.profile__edit');
+
+let popup = document.querySelector('.popup');
+let formElement = popup.querySelector('.popup__form')
+let nameInput = formElement.querySelector('.popup__input_form_name');
+let jobInput = formElement.querySelector('.popup__input_form_job');
+let btnClose = popup.querySelector('.popup__close');
+
 let openPopup = () => {
-  formElement.classList.add('popup_opened');
-}
-btnEdit.addEventListener('click', openPopup);
-
-// закрыть popup
-let btnClose = formElement.querySelector('.popup__close');
-let closePopup = () => {
-  formElement.classList.remove('popup_opened');
-}
-btnClose.addEventListener('click', closePopup);
-
-// добавляем эти данные в поля ввода
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__job');
-if (nameInput.textContent === '') {
+  popup.classList.add('popup_opened');
   nameInput.value = name.textContent;
   jobInput.value = job.textContent;
 }
 
-// считываем новые данные из полей
-let btnSubmit = formElement.querySelector('.popup__submit')
-btnSubmit.addEventListener('click', function() {
+let closePopup = () => {
+  popup.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
   name.textContent = nameInput.value;
   job.textContent = jobInput.value;
   closePopup();
-});
+}
 
+btnEdit.addEventListener('click', openPopup);
+btnClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
 
