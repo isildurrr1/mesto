b0cf0a26e3e9d01c82fc9cbf94827e0eea9b0176
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-02-25 16:33:01 +0700

    close with Escape

--- script/script.js
+++ script/script.js
@@ -142,63 +142,18 @@ btnImageClose.addEventListener('click', () => {
   closePopup(popupImage)
 })
 
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('popup__input_type_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__input-error_active');
-};
+const popups = document.querySelectorAll('.popup');
 
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('popup__input_type_error');
-  errorElement.classList.remove('popup__input-error_active');
-  errorElement.textContent = '';
-};
-
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
+popups.forEach((overlay) => {
+  overlay.addEventListener('mousedown', (evt) => {
+    closePopup(evt.target);
   })
-};
+})
 
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add('popup__submit_not-active');
-  } else {
-    buttonElement.classList.remove('popup__submit_not-active');
+document.addEventListener('keydown', (e) => {
+  if(e.key === 'Escape') {
+    popups.forEach((overlay) => {
+        closePopup(overlay)
+    })
   }
-}
-
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-  const buttonElement = formElement.querySelector('.popup__submit');
-  toggleButtonState(inputList, buttonElement);
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-const enableValidation = () => {
-  let formList = Array.from(document.querySelectorAll('.popup__form'));
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-    });
-    setEventListeners(formElement);
-  });
-}
-
-enableValidation()
-
+})
